961e74aeb333ccbbb305f5ed966fab4c3ebccffc
Thalutn5 <37041254+Thalutn5@users.noreply.github.com>
2019-02-28 23:39:28 +0700

    Update soal3.sh

--- soal3.sh
+++ soal3.sh
@@ -10,7 +10,9 @@ do
 	then
 		fileke=$((fileke + 1))
 	else
-		cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1 > /home/thalutn5/Documents/Soalshift1/nmr3/password$fileke.txt
+		cat /dev/urandom | tr -dc 'a-zA-Z' | head -c 11 >> /home/thalutn5/Documents/Soalshift1/nmr3/password$fileke.txt
+                cat /dev/urandom | tr -dc '0-9' | head -c 1 >> /home/thalutn5/Documents/Soalshift1/nmr3/password$fileke.txt
+
 		##membuat sebuah string | persyaratan | panjangnya 12 | baris |di folder mana
 		i=0
 	fi
